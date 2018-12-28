410a9c7959ad256ad4029a01c12ee6171c79011f
jackhoop <wgqhlzds@gmail.com>
2018-12-28 19:32:11 +0800

    测试

--- template/app.js
+++ template/app.js
@@ -6,15 +6,24 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
+
     // 登录
     wx.login({
       success: res => {
+        console.log("login",res)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log("appsetting",res);
+        if (!res.authSetting['scope.userInfo']){
+          wx.navigateTo({
+            url: '/pages/authentication/index'　　// 登录
+          })
+          return;
+        }
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
--- template/pages/index/index.js
+++ template/pages/index/index.js
@@ -1,56 +1,118 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
+const app = getApp();
 Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    motto: 'Hi 开发者！',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    cardCur: 0,
+    TabCur: 0,
+    scrollLeft: 0,
+    tabs:[{
+      id: 0,
+      name: '附近发现'
+    },{
+        id: 1,
+        name: '人气最高'
+      }],
+    tower: [{
+      id: 0,
+      url: 'https://image.weilanwl.com/img/4x3-1.jpg'
+    }, {
+      id: 1,
+      url: 'https://image.weilanwl.com/img/4x3-2.jpg'
+    }, {
+      id: 2,
+      url: 'https://image.weilanwl.com/img/4x3-3.jpg'
+    }, {
+      id: 3,
+      url: 'https://image.weilanwl.com/img/4x3-4.jpg'
+    }, {
+      id: 4,
+      url: 'https://image.weilanwl.com/img/4x3-2.jpg'
+    }, {
+      id: 5,
+      url: 'https://image.weilanwl.com/img/4x3-4.jpg'
+    }, {
+      id: 6,
+      url: 'https://image.weilanwl.com/img/4x3-2.jpg'
+    }]
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onLoad() {
+    this.towerSwiper('tower');
+    // 初始化towerSwiper 传已有的数组名即可
+  },
+  DotStyle(e) {
+    this.setData({
+      DotStyle: e.detail.value
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  // cardSwiper
+  cardSwiper(e) {
+    this.setData({
+      cardCur: e.detail.current
+    })
+  },
+  // towerSwiper
+  // 初始化towerSwiper
+  towerSwiper(name) {
+    let list = this.data[name];
+    for (let i = 0; i < list.length; i++) {
+      list[i].zIndex = parseInt(list.length / 2) + 1 - Math.abs(i - parseInt(list.length / 2))
+      list[i].mLeft = i - parseInt(list.length / 2)
+    }
+    this.setData({
+      towerList: list
+    })
+  },
+
+  // towerSwiper触摸开始
+  towerStart(e) {
+    this.setData({
+      towerStart: e.touches[0].pageX
+    })
+  },
+
+  // towerSwiper计算方向
+  towerMove(e) {
+    this.setData({
+      direction: e.touches[0].pageX - this.data.towerStart > 0 ? 'right' : 'left'
+    })
+  },
+
+  // towerSwiper计算滚动
+  towerEnd(e) {
+    let direction = this.data.direction;
+    let list = this.data.towerList;
+    if (direction == 'right') {
+      let mLeft = list[0].mLeft;
+      let zIndex = list[0].zIndex;
+      for (let i = 1; i < list.length; i++) {
+        list[i - 1].mLeft = list[i].mLeft
+        list[i - 1].zIndex = list[i].zIndex
       }
+      list[list.length - 1].mLeft = mLeft;
+      list[list.length - 1].zIndex = zIndex;
+      this.setData({
+        towerList: list
+      })
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
+      let mLeft = list[list.length - 1].mLeft;
+      let zIndex = list[list.length - 1].zIndex;
+      for (let i = list.length - 1; i > 0; i--) {
+        list[i].mLeft = list[i - 1].mLeft
+        list[i].zIndex = list[i - 1].zIndex
+      }
+      list[0].mLeft = mLeft;
+      list[0].zIndex = zIndex;
+      this.setData({
+        towerList: list
       })
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  tabSelect(e) {
+    console.log(e);
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      TabCur: e.currentTarget.dataset.id,
+      scrollLeft: (e.currentTarget.dataset.id - 1) * 60
     })
   }
-})
+});
