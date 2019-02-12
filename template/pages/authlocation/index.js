03c9ef8fd9048f5f1a7bf9028127edf4b11319c7
jackhoop <wgqhlzds@gmail.com>
2019-02-12 10:32:15 +0800

    图标

---
+++ template/pages/authlocation/index.js
@@ -0,0 +1,20 @@
+const app = getApp();
+Page({
+  data: {
+    StatusBar: app.globalData.StatusBar,
+    CustomBar: app.globalData.CustomBar,
+  },
+  onLoad() {
+    let that = this;
+    that.handler()
+  },
+  handler: function (data){
+    console.log(data)
+    if (data.detail.authSetting["scope.userLocation"] === true){
+      wx.switchTab({
+        url: '/pages/index/index'　　// 首页
+      })
+    }
+  }
+
+});
